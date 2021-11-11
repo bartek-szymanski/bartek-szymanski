ae8cebf6de6b78c39012f2692747e4305fb22e6a
Bartek Szymanski <bartekszymanskidev@gmail.com>
2021-11-11 18:04:00 +0100

    - Added `postcss-sorting` and custom properties order by type, - Regen CSS,

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,21 @@
+const propertiesOrder = require('./postcss-properties-order.js');
+
 module.exports = {
     plugins: [
         require('postcss-preset-env'),
-    ]
+        [
+            'postcss-sorting',
+            {
+                // order: [
+                //     'custom-properties',
+                //     'dollar-variables',
+                //     'declarations',
+                //     'rules',
+                //     'at-rules',
+                // ],
+                'properties-order': propertiesOrder,
+                // 'unspecified-properties-position': 'bottom',
+            },
+        ]
+    ],
 };
